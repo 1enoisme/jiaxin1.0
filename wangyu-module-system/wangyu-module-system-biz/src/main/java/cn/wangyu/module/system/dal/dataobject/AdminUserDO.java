62e972addab9a532dc6bbef4036bd65c8f3c11fb
wangyu <694540275@qq.com>
2023-03-15 18:10:22 +0800

    shinelon 1.2

--- wangyu-module-system/wangyu-module-system-biz/src/main/java/cn/wangyu/module/system/dal/dataobject/AdminUserDO.java
+++ wangyu-module-system/wangyu-module-system-biz/src/main/java/cn/wangyu/module/system/dal/dataobject/AdminUserDO.java
@@ -1,5 +1,14 @@
 package cn.wangyu.module.system.dal.dataobject;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class AdminUserDO {
 
